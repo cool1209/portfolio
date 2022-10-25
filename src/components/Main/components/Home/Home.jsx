572f6d7e77c4fcb46d6dd38513d44f0558855583
Simba830 <JamesRyan01031@outlook.com>
2022-10-26 01:22:17 +0300

    Fixed home block styles

--- src/components/Main/components/Home/Home.jsx
+++ src/components/Main/components/Home/Home.jsx
@@ -12,7 +12,11 @@ const Home = () => {
             </h1>
 
             <p className={styles.home__text}>
-              I'm a frontend developer with a huge desire to learn new and improve. Currently, I'm looking for a new challenging project to develop myself even more. I have profound knowledge of HTML5/CSS3 including preprocessors (Sass/Less), a good understanding of responsive web design, and proficiency in working with native JavaScript (Typescript) & Frameworks (React, Redux). Also, I'm ready to learn new technologies. I have experience in website layout and developing interfaces for web applications.
+              I'm a frontend developer with a desire to learn new and improve. Currently, I'm looking for a react developer position and new challenging projects to develop myself even more.
+            </p>
+
+            <p className={styles.home__text}>
+              I have profound knowledge of HTML and CSS including preprocessors (Sass/Less), a good understanding of responsive web design, and proficiency in working with native JavaScript (Typescript) & Frameworks (React, Redux, MobX, NextJS). Also, I'm ready to learn new technologies.
             </p>
           </div>
         </div>
