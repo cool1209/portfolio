9c1cefdab70baa545579a4b5dcbd159fb307517f
Simba830 <JamesRyan01031@outlook.com>
2022-10-26 01:47:22 +0300

    Added footer layout

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,9 +4,17 @@ import styles from './Footer.module.scss';
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div className='container'>
-        Footer
+      <div className={styles.footer__copyText}>
+        Copyright - 2022
       </div>
+
+      <a
+        href='https://www.linkedin.com/in/ihor-kuchin-6a3736203/'
+        target={`_blank`}
+        className={styles.footer__copyAuthor}
+      >
+        Ihor-IK
+      </a>
     </footer>
   );
 };
